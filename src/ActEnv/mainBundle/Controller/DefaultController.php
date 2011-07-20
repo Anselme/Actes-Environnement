fa097cc1b1e97de308670f041899c18ef2cca658
Stéphane Girault <stephane.girault@gmail.com>
2011-07-20 22:06:33 +0200

    Pages statiques

--- src/ActEnv/mainBundle/Controller/DefaultController.php
+++ src/ActEnv/mainBundle/Controller/DefaultController.php
@@ -3,13 +3,34 @@
 namespace ActEnv\mainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+// these import the "@Route" and "@Template" annotations
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 
 class DefaultController extends Controller
 {
-    
-    public function indexAction($name)
+
+    public function indexAction()
     {
-        return $this->render('ActEnvmainBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('ActEnvmainBundle:Default:index.html.twig');
     }
+
+    public function activiteAction()
+    {
+        return $this->render('ActEnvmainBundle:Default:activite.html.twig');
+    }
+
+    public function pourquiAction()
+    {
+        return $this->render('ActEnvmainBundle:Default:pourqui.html.twig');
+    }
+
+    public function valorisationAction()
+    {
+        return $this->render('ActEnvmainBundle:Default:valorisation.html.twig');
+    }
+
 }
